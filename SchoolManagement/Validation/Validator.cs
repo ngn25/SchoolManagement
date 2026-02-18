46a336885e729260a2f26977bc7636fa81914d80
neginM <negin.mlh97@gmail.com>
2026-02-18 11:14:55 +0330

    Update:Validation

--- SchoolManagement/Validation/Validator.cs
+++ SchoolManagement/Validation/Validator.cs
@@ -2,31 +2,32 @@ namespace SchoolManagement.Validation
 {
     public static class Validator
     {
-        public static void ValidateEmail(string Email)
+        public static bool ValidateEmail(string Email)
         {
             if (string.IsNullOrWhiteSpace(Email))
-                throw new ArgumentException("Email cannot be empty!");
+                return false;
 
             if (!Email.Contains('@'))
-                throw new ArgumentException("Email must contain '@'");
+                return false;
+
+            return true;
         }
 
-        public static void ValidatePhoneNumber(string PhoneNumber)
+        public static bool ValidatePhoneNumber(string PhoneNumber)
         {
             if (string.IsNullOrWhiteSpace(PhoneNumber))
-                throw new ArgumentException("Phone cannot be empty!");
+                return false;
 
-       
             PhoneNumber = PhoneNumber.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "");
-
-  
             PhoneNumber = PhoneNumber
                 .Replace("۰","0").Replace("۱","1").Replace("۲","2")
                 .Replace("۳","3").Replace("۴","4").Replace("۵","5")
                 .Replace("۶","6").Replace("۷","7").Replace("۸","8").Replace("۹","9");
 
             if (PhoneNumber.Length != 11 || !PhoneNumber.StartsWith("09"))
-                throw new ArgumentException("Phone number must be 11 digits and start with '09'");
+                return false;
+
+            return true;
         }
     }
 }
